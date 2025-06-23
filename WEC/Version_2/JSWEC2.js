db51a2773b84e1f87aa6d9473240c3c85cd654e3
DuvalK78 <duval.kerian18@ik.me>
2025-06-23 10:54:13 +0200

    Update JSWEC2.js

--- WEC/Version_2/JSWEC2.js
+++ WEC/Version_2/JSWEC2.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const teams = [
     { name: "AF Corse", link: "../WEC_Teams/AF_Corse.html" },
-    { name: "Alpine Endurance Team", link: "../WEC_Teams/Alpine.html" },
-    { name: "BMW M Team WRT", link: "../WEC_Teams/BMW.html" },
-    { name: "Cadillac Racing", link: "../WEC_Teams/Cadillac.html" },
-    { name: "Ferrari AF Corse", link: "../WEC_Teams/Ferrari.html" },
-    { name: "Hertz Team Jota", link: "../WEC_Teams/Jota.html" },
-    { name: "Isotta Fraschini", link: "../WEC_Teams/Isotta.html" },
-    { name: "Lamborghini Iron Lynx", link: "../WEC_Teams/Lamborghini.html" },
-    { name: "Peugeot TotalEnergies", link: "../WEC_Teams/Peugeot.html" },
-    { name: "Porsche Penske Motorsport", link: "../WEC_Teams/Porsche_Penske.html" },
-    { name: "Proton Competition", link: "../WEC_Teams/Proton.html" },
-    { name: "Toyota Gazoo Racing", link: "../WEC_Teams/Toyota.html" }
+    { name: "Alpine Endurance Team", link: "../WEC_Teams/Alpine_Endurance_Team.html" },
+    { name: "Aston Martin", link: "../WEC_Teams/Aston_Martin.html" },
+    { name: "BMW M Team WRT", link: "../WEC_Teams/BMW_M_Team_WRT.html" },
+    { name: "Cadillac Racing", link: "../WEC_Teams/Cadillac_Racing.html" },
+    { name: "Ferrari AF Corse", link: "../WEC_Teams/Ferrari_AF_Corse.html" },
+    { name: "Peugeot TotalEnergies", link: "../WEC_Teams/Peugeot_TotalEnergies.html" },
+    { name: "Porsche Penske Motorsport", link: "../WEC_Teams/Porsche_Penske_Motorsport.html" },
+    { name: "Proton Competition", link: "../WEC_Teams/Proton_Competition.html" },
+    { name: "Toyota Gazoo Racing", link: "../WEC_Teams/Toyota_Gazoo_Racing.html" }
   ];
 
   let currentIndex = 0;
